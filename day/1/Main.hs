a20e9df4c459126dd2a5e0db830fd8c9481e0772
KSPAtlas <kspatlas@disroot.org>
2024-12-01 15:03:19 +0000

    day 1

---
+++ day/1/Main.hs
@@ -0,0 +1,16 @@
+module Main (main) where
+
+import System.IO
+import Data.List
+
+main :: IO ()
+main = do
+  handle <- openFile "input.txt" ReadMode
+  rawContents <- hGetContents handle
+  let contents = map words $ lines rawContents
+      (leftid, rightid) = (map (read . head) contents, map (read . last) contents)
+      sorted_contents = zip (sort leftid) (sort rightid)
+      differences = map (abs . uncurry (-)) sorted_contents
+      total = sum differences
+
+  putStrLn $ "The toal sum is " ++ show total
