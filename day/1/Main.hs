7fe38e37f370d2e3fe1e50a5017b91e4744616bb
KSPAtlas <kspatlas@disroot.org>
2024-12-01 15:42:29 +0000

    complete part 2 of day 1

--- day/1/Main.hs
+++ day/1/Main.hs
@@ -3,15 +3,29 @@ module Main (main) where
 import System.IO
 import Data.List
 
+countOccurences :: (Eq a) => [a] -> a -> Int
+countOccurences [] _ = 0
+countOccurences xs f = length ys
+    where ys = [x | x <- xs, x == f]
+
 main :: IO ()
 main = do
   handle <- openFile "input.txt" ReadMode
   rawContents <- hGetContents handle
+
+  -- Part 1
   let contents = map words $ lines rawContents
       (leftid, rightid) = (map (read . head) contents, map (read . last) contents)
       sorted_contents = zip (sort leftid) (sort rightid)
       differences = map (abs . uncurry (-)) sorted_contents
       total = sum differences
 
-  putStrLn $ "The toal sum is " ++ show total
+  putStrLn $ "The total sum is " ++ show total
+
+  -- Part 2
+  let occurences = map (countOccurences rightid) leftid
+      scores = zipWith (*) leftid occurences
+      total2 = sum scores
+
+  putStrLn $ "The similarity score is " ++ show total2
   hClose handle
